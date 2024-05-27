cb3a6603f458011349e68e438cd9336eea04772c
unknown <zeeshannoorofficial@gmail.com>
2024-05-27 17:56:20 +0500

    Task3 faqs

--- script.js
+++ script.js
@@ -124,3 +124,86 @@ let ApiFun=()=>{
 }
 ApiFun();
 
+//Task 3 Faqs
+const faqs = [
+    {
+      title: "Where are these chairs assembled?",
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+    },
+    {
+      title: "How long do I have to return my chair?",
+      text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+    },
+    {
+      title: "Do you ship to countries outside the EU?",
+      text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+    },
+  ];
+
+  
+  for(let i=0;i<faqs.length;i++){
+    let mainDiv=document.createElement("div");
+    mainDiv.classList.add('faqDiv');
+     
+    let headDiv=document.createElement("div");
+    headDiv.classList.add('headDiv');
+    let numEle=document.createElement("h2");
+    numEle.innerText=`0${i+1}`;
+    numEle.classList.add("numEle");
+    numEle.style.color="gray";
+    let headEle=document.createElement("h3");
+    headEle.innerText=faqs[i].title;
+    let sign=document.createElement('h2');
+    sign.classList.add('sign');
+    sign.innerText="+";
+    headDiv.appendChild(numEle);
+    headDiv.appendChild(headEle);
+    headDiv.appendChild(sign);
+  
+  
+    let paraEle=document.createElement("p");
+    paraEle.innerText=faqs[i].text;
+    paraEle.style.display="none";
+  
+    mainDiv.appendChild(headDiv);
+    mainDiv.appendChild(paraEle);
+    document.getElementById('faqs').appendChild(mainDiv);
+  };
+  let faqDiv=document.querySelectorAll('.faqDiv');
+  let toggle=true;
+  faqDiv.forEach(ele=>{
+ 
+    ele.addEventListener('click',()=>{
+      
+     
+        const allParas = document.querySelectorAll('#faqs p');
+        allParas.forEach(p => p.style.display = 'none');
+        let para = ele.querySelector('p');
+
+        const allMainDiv=document.querySelectorAll(".faqDiv");
+        allMainDiv.forEach(mainDiv=>mainDiv.style.borderTop="none");
+        ele.style.borderTop='5px solid green';
+
+        const allNumEle=document.querySelectorAll('.numEle');
+        allNumEle.forEach(numEle=>numEle.style.color="gray");
+
+        let selectNumEle=ele.querySelector('.numEle');
+        selectNumEle.style.color="green";
+
+        const allSign=document.querySelectorAll('.sign');
+        allSign.forEach(sign=>sign.innerText='+');
+
+        let selectSign=ele.querySelector('.sign');
+        selectSign.innerText="-";
+        if(para.style.display=="block" ){
+            para.style.display="none"
+            toggle=true;
+            
+        }else{
+            para.style.display="block";
+            toggle=false;
+        }
+     
+    });
+    
+  })
